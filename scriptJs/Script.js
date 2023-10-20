157421a5c8249276b51df648139a3471f1fdea31
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2023-10-20 15:43:40 +0600

    Add reviewe section

--- scriptJs/Script.js
+++ scriptJs/Script.js
@@ -34,3 +34,33 @@
 //     wrapAround: true,
 //   });
 // });
+
+// For Video play
+
+new VenoBox({
+  selector: ".my-video-links",
+});
+
+// For counter up
+$(".count-up").each(function () {
+  var $this = $(this),
+    countTo = $this.attr("data-count");
+
+  $({ countNum: $this.text() }).animate(
+    {
+      countNum: countTo,
+    },
+
+    {
+      duration: 8000,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function () {
+        $this.text(this.countNum);
+        //alert('finished');
+      },
+    }
+  );
+});
